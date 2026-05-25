98cb867f63e886220e21fccf7e585e02921cac4f
arnaldofarias312-source <arnaldofarias312@gmail.com>
2026-05-25 19:31:56 -0400

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,5 +1,23 @@
 import streamlit as st
 
+# ---- CONFIGURACIÓN DE LA PÁGINA ----
+st.set_page_config(page_title="Mi Primera App", page_icon="⚽", layout="centered")
+
+# ---- TRUCO PARA OCULTAR ELEMENTOS DE STREAMLIT ----
+hide_style = """
+    <style>
+    /* Ocultar el botón de GitHub (Fork) en la esquina superior derecha */
+    .stAppDeployButton {visibility: hidden;}
+    
+    /* Ocultar la barra de menú superior y el ícono de GitHub */
+    header {visibility: hidden;}
+    
+    /* Ocultar el pie de página de Streamlit de abajo */
+    footer {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_style, unsafe_allow_html=True)
+
 # Título de la página web
 st.title("⚽ ¡Mi Primera App Interactiva!")
 
